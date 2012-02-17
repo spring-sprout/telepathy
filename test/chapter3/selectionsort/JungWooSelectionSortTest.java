bbc4ac18a5147d42c376d496cc0082e10e680d7b
Kim Jung-Woo <kmiracle@gmail.com>
2012-02-17 22:18:41 +0900

    테스트 코드 리펙토링

--- test/chapter3/selectionsort/JungWooSelectionSortTest.java
+++ test/chapter3/selectionsort/JungWooSelectionSortTest.java
@@ -1,26 +1,46 @@
 package chapter3.selectionsort;
 
+import chapter3.BaseSortTest;
 import org.junit.Test;
 
-import java.util.Arrays;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertArrayEquals;
 
 
-public class JungWooSelectionSortTest {
-    private int[] input = new int[]{3, 2, 1, 5, 10, 2};
-    private final int[] output = new int[]{1, 2, 2, 3, 5, 10};
+public class JungWooSelectionSortTest extends BaseSortTest {
 
     @Test
-    public void testSort() throws Exception {
+    public void testSort_무작위_정렬된_배열() throws Exception {
         // Given
         JungWooSelectionSort sort = new JungWooSelectionSort();
 
         // When
-        sort.sort(input);
+        sort.sort(정렬되지않은_배열);
 
         // Then
-        assertThat(Arrays.equals(input, output), is(true));
+        assertArrayEquals(정렬되지않은_배열, 정렬_결과);
+    }
+
+    @Test
+    public void testSort_역정렬된_배열() throws Exception {
+        // Given
+        JungWooSelectionSort sort = new JungWooSelectionSort();
+
+        // When
+        sort.sort(역정렬된_배열);
+
+        // Then
+        assertArrayEquals(역정렬된_배열, 정렬_결과);
+    }
+
+    @Test
+    public void testSort_정렬된_배열() throws Exception {
+        // Given
+        JungWooSelectionSort sort = new JungWooSelectionSort();
+
+        // When
+        sort.sort(정렬된_배열);
+
+        // Then
+        assertArrayEquals(정렬된_배열, 정렬_결과);
     }
 }
