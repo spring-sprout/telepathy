6d12fdb1cf6b90409396425624fdaed6e2b87b6b
Daewon Hyun <hdw822@gmail.com>
2012-02-19 18:21:27 +0900

    퀴즈01 풀이 관련 오답 주석 추가

--- src/quize/number01/DaewonRankingService.java
+++ src/quize/number01/DaewonRankingService.java
@@ -2,6 +2,10 @@ package quize.number01;
 
 public class DaewonRankingService implements RankingService {
 
+	/**
+	 * 3항 연산자를 이용하여 비교문을 사용하였기 때문에 오답 풀이임
+	 * 그런데 딱히 좋은 알고리즘이 떠 오르지 않는다. ㅠ.ㅠ
+	 */
 	@Override
 	public int[] rank(int[] scores) {
 		int[] ranking = new int[scores.length];
