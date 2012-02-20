dd9202de35228cc2744c47cf1482521465f1b08a
Daewon Hyun <hdw822@gmail.com>
2012-02-20 10:14:52 +0900

    퀴즈1 문제 풀이 수정

--- src/quize/number01/DaewonRankingService.java
+++ src/quize/number01/DaewonRankingService.java
@@ -1,23 +1,51 @@
 package quize.number01;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DaewonRankingService implements RankingService {
 
+	private List<List<Integer>> scoreStoreList;
+	private Map<Integer, Integer> rankingMap = new HashMap<Integer, Integer>();
+
+	public DaewonRankingService() {
+		scoreStoreList = new ArrayList<List<Integer>>(101);
+		for (int i = 0; i < 101; i++) {
+			scoreStoreList.add(new ArrayList<Integer>());
+		}
+	}
+
 	/**
-	 * 3항 연산자를 이용하여 비교문을 사용하였기 때문에 오답 풀이임
-	 * 그런데 딱히 좋은 알고리즘이 떠 오르지 않는다. ㅠ.ㅠ
+	 * Radix 정렬 알고리즘에서 힌트를 얻어서 퀴즈문제를 풀었다
+	 * 그런데 풀고나서 기선님 풀이와 비교하니 별반 드릴게 없는 것 같다. ㅡㅡ;;
+	 * 마지막 정렬 알고리즘을 공부하면 O(n)의 복잡도를 갖는 알고리즘을 알 수 있을까???
 	 */
 	@Override
 	public int[] rank(int[] scores) {
 		int[] ranking = new int[scores.length];
 
 		for (int i = 0; i < scores.length; i++) {
-			int value = scores[i];
-			int rank = 1;
-			for (int j = 0; j < scores.length; j++) {
-				rank += (value < scores[j] ? 1 : 0);
+			int score = scores[i];
+			scoreStoreList.get(score).add(score);
+		}
+
+		int rank = 1;
+		for(int i = scoreStoreList.size()-1; i >= 0; i--) {
+			List<Integer> scoreStore = scoreStoreList.get(i);
+			for (Integer score : scoreStore) {
+				rankingMap.put(score, rank);
+				System.out.printf("Score:%d, Rank:%d\n", score, rank);
+				rank += scoreStore.size();
+				break;
 			}
 
-			ranking[i] = rank;
+		}
+
+		for (int i = 0; i < scores.length; i++) {
+			int score = scores[i];
+			ranking[i] = rankingMap.get(score);
 		}
 
 		return ranking;
